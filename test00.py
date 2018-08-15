0e95a5c473e14c0f89af60d4728f2cc83aa8b5cf
yanzhp <40047931+yanzhp@users.noreply.github.com>
2018-08-15 21:31:12 +0800

    Delete test00.py

--- test00.py
+++
@@ -1,8 +0,0 @@
-words = []
-counts = []
-for line in open('词频统计(去停用词).txt',encoding='utf-8'):
-        line.strip('\n')
-        # print(len(line.split()))
-        if len(line.split()) > 1:
-                print(line.split(' ')[0])
-                print(int(line.split(' ')[1].strip('\n')))
